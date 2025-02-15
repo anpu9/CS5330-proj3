26ca04c358add5d75fd6834dc38f96762a2fe078
yuyang26 <tian.yuyan@northeastern.edu>
2025-02-14 22:03:50 -0800

    header files for task4, feature extraction

---
+++ include/obb_feature_extraction.h
@@ -0,0 +1,10 @@
+/*
+ * Authors: ${AUTHORS}
+ * Date: 2025/2/14
+ * Purpose: computing Rotation, translation, scale invariant features of a bounding box.
+ */
+
+#ifndef PROJ3_OBB_FEATURE_EXTRACTION_H
+#define PROJ3_OBB_FEATURE_EXTRACTION_H
+
+#endif //PROJ3_OBB_FEATURE_EXTRACTION_H
---
+++ src/obb_feature_extraction.cpp
@@ -0,0 +1,7 @@
+/*
+ * Authors: ${AUTHORS}
+ * Date: 2025/2/14
+ * Purpose: ${PURPOSE}
+ */
+
+#include "../include/obb_feature_extraction.h"
