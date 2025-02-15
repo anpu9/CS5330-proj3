7a5682f6b712edc80ab20ce4b577054be271ca19
yuyang26 <tian.yuyan@northeastern.edu>
2025-02-15 15:23:42 -0800

    1. Add both 4 and 8 connectivity for two-pass segmentation 2. reduce noise by minRegionSize

--- include/image_process.h
+++ include/image_process.h
@@ -15,8 +15,10 @@ using namespace std;
 int threshold(const Mat& src, Mat& dst);
 // Clean up your thresholded image with morphological filtering
 int morphologicalFilter(const Mat& src, Mat& dst);
-// Segment the image into regions
-int two_pass_segmentation(const Mat& binaryImage, Mat& regionMap);
+// Two-pass segmentation the image into regions ignoring area smaller than minRegionSize, with 4-connectivity
+int two_pass_segmentation_4conn(const Mat& binaryImage, Mat& regionMap, int minRegionSize = 50);
+// Two-pass segmentation the image into regions ignoring area smaller than minRegionSize, with 8-connectivity
+int two_pass_segmentation_8conn(const Mat& binaryImage, Mat& regionMap, int minRegionSize = 50);
 // Helper function to visualize the segmentation result
 int colorizeRegions(const cv::Mat& labelMap, Mat& colorImage);
 #endif //PROJ3_IMAGE_PROCESS_H
